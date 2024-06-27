f86141effa6ce927b47e121d2d645ae5c73e87f0
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-06-28 01:32:40 +0900

    py added

---
+++ bs4/bs4_beginning.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+website= 'https://subslikescript.com/movie/Titanic-120338'
+# website   = 'https://imginn.com/ssahn01/'
+result = requests.get(website)
+content = result.text
+
+soup = BeautifulSoup(content, 'lxml')
+# print( soup.prettify())
+
+box = soup.find('article', class_='main-article')
+
+title = box.find('h1').get_text()
+transcript = box.find('div', class_='full-script').get_text(strip = True, separator=' ')
+# test
+print(title)
+
+# print( '---') test
+print(transcript)
