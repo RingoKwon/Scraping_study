127f87110c539e9e09e1ba5b8f87c0c5a8b300c2
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-06-28 23:43:57 +0900

    chrome driver is not working

--- selenium/selenium_study.py
+++ selenium/selenium_study.py
@@ -1,2 +1,8 @@
-# test
-# ddd
+from selenium import webdriver
+
+website = 'https://www.adamchoi.co.uk/overs/detailed' # https://www.adamchoi.co.uk/overs/detailed
+# /Users/ringokwon/Project/python_project/scraping_study2/chromedriver
+path = '/Users/ringokwon/Project/python_project/scraping_study2/selenium/chromedriver'
+driver = webdriver.Chrome(path)
+driver.get(website)
+print(driver.page_source)
