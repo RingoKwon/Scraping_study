891f728d63d43aba3a19c98ca159ea5376c6e163
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-06-29 00:06:42 +0900

    chromedriver solution

--- selenium/selenium_study.py
+++ selenium/selenium_study.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service # add by ringo
 
 website = 'https://www.adamchoi.co.uk/overs/detailed' # https://www.adamchoi.co.uk/overs/detailed
 # /Users/ringokwon/Project/python_project/scraping_study2/chromedriver
 path = '/Users/ringokwon/Project/python_project/scraping_study2/selenium/chromedriver'
-driver = webdriver.Chrome(path)
+driver = webdriver.Chrome( service=Service(  path)) #add by ringo
 driver.get(website)
-print(driver.page_source)
+
+# time.sleep(10)
+while True: # add by ringo
+    pass
