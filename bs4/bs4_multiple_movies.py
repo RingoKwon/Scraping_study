f406e03a8c0de2ed715762d39a6e3f71d4afb635
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-06-28 14:16:25 +0900

    Update bs4_multiple_movies.py
    mutipage completed

--- bs4/bs4_multiple_movies.py
+++ bs4/bs4_multiple_movies.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-website= 'https://subslikescript.com/movie/Titanic-120338'
+root = 'https://subslikescript.com'
+website = f'{root}/movies'
 # website   = 'https://imginn.com/ssahn01/'
 result = requests.get(website)
 content = result.text
@@ -9,11 +10,24 @@ content = result.text
 soup = BeautifulSoup(content, 'lxml')
 # print( soup.prettify())
 
+links = []
 box = soup.find('article', class_='main-article')
+a_all = box.find_all('a', href=True)
+for link in a_all:
+    links.append(link['href'])
 
-title = box.find('h1').get_text()
-transcript = box.find('div', class_='full-script').get_text(strip = True, separator=' ')
+links_test = links[:5]
+print(links_test)
 
-with open ( f'{title}.txt', 'w') as file:
-    file.write(transcript)
+for link in links_test:
+    website = f'{root}/{link}'
+    result = requests.get(website)
+    content = result.text
+    soup = BeautifulSoup(content, 'lxml')
+    box = soup.find('article', class_='main-article')
 
+    title = box.find('h1').get_text()
+    transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+
+    with open(f'{title}.txt', 'w') as file:
+        file.write(transcript)
