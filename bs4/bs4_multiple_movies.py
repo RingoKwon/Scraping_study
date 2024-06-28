5c6da57b50b226d75268f79046a85e6d88f8bb10
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-06-28 16:32:24 +0900

    Update bs4_multiple_movies.py
    folder updated

--- bs4/bs4_multiple_movies.py
+++ bs4/bs4_multiple_movies.py
@@ -1,8 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+import os
 
 root = 'https://subslikescript.com'
-website = f'{root}/movies'
+website = f'{root}/movies_letter-A'
 # website   = 'https://imginn.com/ssahn01/'
 result = requests.get(website)
 content = result.text
@@ -10,6 +11,12 @@ content = result.text
 soup = BeautifulSoup(content, 'lxml')
 # print( soup.prettify())
 
+#  pagenation
+pagenation = soup.find('ul', class_='pagination')
+pages  = pagenation.find_all('li', class_='page-item')
+last_page = pages[-2].text
+# print(last_page)
+#
 links = []
 box = soup.find('article', class_='main-article')
 a_all = box.find_all('a', href=True)
@@ -17,7 +24,7 @@ for link in a_all:
     links.append(link['href'])
 
 links_test = links[:5]
-print(links_test)
+
 
 for link in links_test:
     website = f'{root}/{link}'
@@ -29,5 +36,7 @@ for link in links_test:
     title = box.find('h1').get_text()
     transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
 
-    with open(f'{title}.txt', 'w') as file:
+    rel_path = '/Users/ringokwon/Project/python_project/scraping_study2/bs4/files_bs4'
+    with open(os.path.join(rel_path, f'{title}.txt'), 'w') as file:
         file.write(transcript)
+
