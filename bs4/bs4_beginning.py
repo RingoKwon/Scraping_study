cba530a47784c5afccd097a327ecb707bd129cce
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-06-28 13:21:35 +0900

    mutipage scraping solution updated

--- bs4/bs4_beginning.py
+++ bs4/bs4_beginning.py
@@ -13,8 +13,7 @@ box = soup.find('article', class_='main-article')
 
 title = box.find('h1').get_text()
 transcript = box.find('div', class_='full-script').get_text(strip = True, separator=' ')
-# test
-print(title)
 
-# print( '---')
-print(transcript)
+with open ( f'{title}.txt', 'w') as file:
+    file.write(transcript)
+
