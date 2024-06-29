8a4da7dbd3037ff8cc622e41957d4a681a8bbcc9
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-06-29 12:17:09 +0900

    brave use

--- Insta_img_bulk/main.py
+++ Insta_img_bulk/main.py
@@ -1,21 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
 
 print(webdriver.__version__)
 website = 'https://imginn.com/ssahn01/'
 path = '/Users/ringokwon/Project/python_project/scraping_study2/chromedriver'
 
-# Create a Chrome Options object
-chrome_options = Options()
-chrome_options.add_argument("--disable-popup-blocking")
-chrome_options.add_argument("--disable-notifications")  # To disable notifications
-chrome_options.add_argument("--disable-infobars")  # To disable infobars
+options = Options()
+options.binary_location = '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser'
 
-driver = webdriver.Chrome(service=Service(path), options=chrome_options)
+driver = webdriver.Chrome(service=Service(path), options=options)
 
 driver.get(website)
 
@@ -26,6 +23,17 @@ try:
     all_matches_button.click()
 except Exception as e:
     print(f"An error occurred: {e}")
+
+# Handle pop-up ads
+try:
+    # Switch to the new window
+    driver.switch_to.window(driver.window_handles[-1])
+    # Close the new window
+    driver.close()
+    # Switch back to the main window
+    driver.switch_to.window(driver.window_handles[0])
+except Exception as e:
+    print(f"An error occurred while handling pop-up ads: {e}")
 
 try:
     while True:
