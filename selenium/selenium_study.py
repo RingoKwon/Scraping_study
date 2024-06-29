0340c93376f5be585de53f559f66d6d980fa6500
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-06-29 11:17:23 +0900

    button clicck and quite when i quit

--- selenium/selenium_study.py
+++ selenium/selenium_study.py
@@ -1,12 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service # add by ringo
-
+print(webdriver.__version__)
 website = 'https://www.adamchoi.co.uk/overs/detailed' # https://www.adamchoi.co.uk/overs/detailed
 # /Users/ringokwon/Project/python_project/scraping_study2/chromedriver
 path = '/Users/ringokwon/Project/python_project/scraping_study2/selenium/chromedriver'
 driver = webdriver.Chrome( service=Service(  path)) #add by ringo
 driver.get(website)
 
-# time.sleep(10)
-while True: # add by ringo
+# all_matches_button = driver.find_element_by_xpath('//label[@analytics-event="All matches"]')
+all_matches_button = driver.find_element('xpath', '//label[@analytics-event="All matches"]')
+all_matches_button.click()
+
+
+# driver.quit()
+# # time.sleep(10)
+try:
+    while True: # add by ringo
+        driver.title
+except:
     pass
