d6124de3c6d849aaaa713588415472f2bdfa5fed
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-06-29 11:23:40 +0900

    chrome driver location changed

--- selenium/selenium_study.py
+++ selenium/selenium_study.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.service import Service # add by ringo
 print(webdriver.__version__)
 website = 'https://www.adamchoi.co.uk/overs/detailed' # https://www.adamchoi.co.uk/overs/detailed
 # /Users/ringokwon/Project/python_project/scraping_study2/chromedriver
-path = '/Users/ringokwon/Project/python_project/scraping_study2/selenium/chromedriver'
+path = '/Users/ringokwon/Project/python_project/scraping_study2/chromedriver'
 driver = webdriver.Chrome( service=Service(  path)) #add by ringo
 driver.get(website)
 
